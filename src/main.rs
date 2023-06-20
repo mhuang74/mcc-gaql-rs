c8dcf5b47b3c918034281479ac59702132e40b22
Michael Huang <mhuang@marinsoftware.com>
2023-06-21 04:03:42 +0800

    upgrade to google ads api v14

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use anyhow::{Context, Result};
 use futures::{stream::FuturesUnordered, StreamExt};
 
 use googleads::GoogleAdsAPIAccess;
-use googleads_rs::google::ads::googleads::v11::services::google_ads_service_client::GoogleAdsServiceClient;
+use googleads_rs::google::ads::googleads::v14::services::google_ads_service_client::GoogleAdsServiceClient;
 use polars::prelude::*;
 use thousands::Separable;
 use tonic::{codegen::InterceptedService, transport::Channel};
